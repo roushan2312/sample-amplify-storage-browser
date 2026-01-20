f2e3cfb0c0cee22a2e4e638d5c4998207e938322
roushan2312 <barnwal23roushan@gmail.com>
2026-01-20 12:19:40 +0530

    change 2

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -18,22 +18,22 @@ export const storage = defineStorage({
    })
 });
 
-export const secondaryStorage = defineStorage({
-  name: 'junk-update-file',
-   access: (allow) => ({
-    'backup_public/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    'backup_admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'backup_private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-   })
-});
+// export const secondaryStorage = defineStorage({
+//   name: 'junk-update-file',
+//    access: (allow) => ({
+//     'backup_public/*': [
+//         allow.guest.to(['read', 'write']),
+//         allow.authenticated.to(['read', 'write', 'delete']),
+//     ],
+//     'backup_admin/*': [
+//         allow.groups(['admin']).to(['read', 'write', 'delete']),
+//         allow.authenticated.to(['read'])
+//     ],
+//     'backup_private/{entity_id}/*': [
+//         allow.entity('identity').to(['read', 'write', 'delete'])
+//     ]
+//    })
+// });
 
 
 
