5e32fc268fb0b762e9e71245fb565f4260a35fbd
roushan2312 <barnwal23roushan@gmail.com>
2026-01-20 12:25:49 +0530

    change 3

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -1,20 +1,20 @@
 import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
-  name: 'kyc-test-upload-file',
+  name: 's3-test-bucket',
   isDefault: true,
    access: (allow) => ({
-    'public/*': [
+    'invoices/*': [
         allow.guest.to(['read', 'write']),
         allow.authenticated.to(['read', 'write', 'delete']),
     ],
-    'admin/*': [
-        allow.groups(['admin']).to(['read', 'write', 'delete']),
-        allow.authenticated.to(['read'])
-    ],
-    'private/{entity_id}/*': [
-        allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
+    // 'admin/*': [
+    //     allow.groups(['admin']).to(['read', 'write', 'delete']),
+    //     allow.authenticated.to(['read'])
+    // ],
+    // 'private/{entity_id}/*': [
+    //     allow.entity('identity').to(['read', 'write', 'delete'])
+    // ]
    })
 });
 
