2a2cd070c782d8496940ac9ff16f0201f73ed993
roushan2312 <barnwal23roushan@gmail.com>
2026-01-21 12:07:01 +0530

    group testing done

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -5,14 +5,15 @@ export const storage = defineStorage({
 //   existing: true,
   isDefault: true,
   access: (allow) => ({
-    'invoices/*': [
-        allow.guest.to(['read', 'write']),
-        allow.authenticated.to(['read', 'write', 'delete']),
-    ],
-    // 'admin/*': [
-    //     allow.groups(['admin']).to(['read', 'write', 'delete']),
-    //     allow.authenticated.to(['read'])
+    // 'invoices/*': [
+    //     allow.guest.to(['read', 'write']),
+    //     allow.authenticated.to(['read', 'write', 'delete']),
     // ],
+    'invoices/*': [
+        allow.groups(['1vp-ops']).to(['read', 'write']),
+        allow.groups(['admin']).to(['read', 'write', 'delete']),
+        allow.authenticated.to(['read', 'write', 'delete']),
+    ]
     // 'private/{entity_id}/*': [
     //     allow.entity('identity').to(['read', 'write', 'delete'])
     // ]
